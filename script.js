8552627a5d7d4a3ede87baa4844a8e4a2be93843
Hrithik Kedare <kedarehrithik@gmail.com>
2024-06-12 10:24:34 +0530

    added-loader

---
+++ script.js
@@ -0,0 +1,9 @@
+function loaderFunction() {
+    setTimeout(showPage, 3000);
+  }
+  
+  function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("loaderDIV").style.display = "block";
+  }
+  
